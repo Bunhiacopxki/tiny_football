79fb8ad31dfdb371eb2694a95e28ce40cf29e3ca
Bunhiacopxki <thinh.nguyen04@hcmut.edu.vn>
2025-02-21 23:05:15 +0700

    ver1

--- 26_motion.cpp
+++ 26_motion.cpp
@@ -6,10 +6,14 @@ and may not be redistributed without written permission.*/
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib> 
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 780;
+int direction = -1;
 
 //Texture wrapper class
 class LTexture
@@ -96,18 +100,21 @@ class Dot
 		//The dimensions of the dot
 		static const int DOT_WIDTH = 20;
 		static const int DOT_HEIGHT = 20;
+		static const int RANGE = 200;
 
 		//Maximum axis velocity of the dot
-		static const int DOT_VEL = 10;
+		static const int DOT_VEL = 5;
+		static const int DOT_VEL_SLOW = 5;
+		
 
 		//Initializes the variables
-		Dot();
+		Dot(bool isMainDot = false);
 
 		//Takes key presses and adjusts the dot's velocity
 		void handleEvent( SDL_Event& e );
 
 		//Moves the dot
-		void move();
+		void move(Dot &mainDot, std::vector<Dot> &otherDots);
 
 		//Shows the dot on the screen
 		void render();
@@ -118,6 +125,7 @@ class Dot
 
 		//The velocity of the dot
 		int mVelX, mVelY;
+		bool mIsMainDot;  // Xác định dot chính
 };
 
 //Starts up SDL and creates window
@@ -286,30 +294,57 @@ int LTexture::getHeight()
 	return mHeight;
 }
 
-
-Dot::Dot()
+double getDistance(int x1, int y1, int x2, int y2)
 {
-    //Initialize the offsets
-    mPosX = 0;
-    mPosY = 0;
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+Dot::Dot(bool isMainDot)
+{
+    // Đặt vị trí ngẫu nhiên trên màn hình cho dot phụ
+    if (!isMainDot)
+    {
+        mPosX = rand() % (SCREEN_WIDTH - DOT_WIDTH);
+        mPosY = rand() % (SCREEN_HEIGHT - DOT_HEIGHT);
+    }
+    else
+    {
+        mPosX = SCREEN_WIDTH / 2;  // Dot chính ở giữa màn hình
+        mPosY = SCREEN_HEIGHT / 2;
+    }
 
     //Initialize the velocity
     mVelX = 0;
     mVelY = 0;
+	mIsMainDot = isMainDot;
 }
 
 void Dot::handleEvent( SDL_Event& e )
 {
+	if (!mIsMainDot) return; // Chỉ dot chính nhận input
+
     //If a key was pressed
 	if( e.type == SDL_KEYDOWN && e.key.repeat == 0 )
     {
         //Adjust the velocity
         switch( e.key.keysym.sym )
         {
-            case SDLK_UP: mVelY -= DOT_VEL; break;
-            case SDLK_DOWN: mVelY += DOT_VEL; break;
-            case SDLK_LEFT: mVelX -= DOT_VEL; break;
-            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+            case SDLK_UP: 
+				mVelY -= DOT_VEL;
+				direction = 0;
+				break;
+            case SDLK_DOWN: 
+				mVelY += DOT_VEL;
+				direction = 0;
+				break;
+            case SDLK_LEFT: 
+				mVelX -= DOT_VEL; 
+				direction = 1;
+				break;
+            case SDLK_RIGHT: 
+				mVelX += DOT_VEL;
+				direction = 1;
+				break;
         }
     }
     //If a key was released
@@ -323,29 +358,108 @@ void Dot::handleEvent( SDL_Event& e )
             case SDLK_LEFT: mVelX += DOT_VEL; break;
             case SDLK_RIGHT: mVelX -= DOT_VEL; break;
         }
+		direction = -1;
     }
 }
 
-void Dot::move()
+void Dot::move(Dot &mainDot, std::vector<Dot> &dots)
 {
-    //Move the dot left or right
-    mPosX += mVelX;
+    if (mIsMainDot) {
+        // Cập nhật vị trí của main dot theo điều khiển
+        mPosX += mVelX;
+        mPosY += mVelY;
 
-    //If the dot went too far to the left or right
-    if( ( mPosX < 0 ) || ( mPosX + DOT_WIDTH > SCREEN_WIDTH ) )
-    {
-        //Move back
-        mPosX -= mVelX;
+        // Kiểm tra va chạm biên
+        if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH)) mPosX -= mVelX;
+        if ((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT)) mPosY -= mVelY;
     }
-
-    //Move the dot up or down
-    mPosY += mVelY;
-
-    //If the dot went too far up or down
-    if( ( mPosY < 0 ) || ( mPosY + DOT_HEIGHT > SCREEN_HEIGHT ) )
+    else
     {
-        //Move back
-        mPosY -= mVelY;
+        // Nếu main dot đứng yên, dot phụ cũng đứng yên
+        if (mainDot.mVelX == 0 && mainDot.mVelY == 0) {
+            return;
+        }
+
+        double distance = getDistance(mPosX, mPosY, mainDot.mPosX, mainDot.mPosY);
+		double speed = sqrt(mainDot.mVelX * mainDot.mVelX + mainDot.mVelY * mainDot.mVelY);
+
+        if (distance < RANGE) {  
+            // Nếu dot phụ quá gần main dot, né ra
+            double dx = (mPosX - mainDot.mPosX) / distance;
+            double dy = (mPosY - mainDot.mPosY) / distance;
+
+			double escapeSpeed = speed * 1.5;
+
+            mVelX += (dx * escapeSpeed - mVelX) * 0.3;	
+			mVelY += (dy * escapeSpeed - mVelY) * 0.3;
+
+			mPosX += mVelX * 0.9;
+			mPosY += mVelY * 0.9;
+
+        }
+        else if (distance > RANGE) {
+            // Nếu nằm ngoài RANGE, di chuyển lại gần main dot
+            if (mPosX < mainDot.mPosX) mPosX += DOT_VEL_SLOW;
+            else if (mPosX > mainDot.mPosX) mPosX -= DOT_VEL_SLOW;
+    
+            if (mPosY < mainDot.mPosY) mPosY += DOT_VEL_SLOW;
+            else if (mPosY > mainDot.mPosY) mPosY -= DOT_VEL_SLOW;
+        }
+        else {
+            // Nếu nằm trong RANGE, di chuyển theo góc phần tư tương ứng
+            double angle = 0;
+
+            if (mainDot.mVelY < 0) { // Main dot đi lên
+                if (mPosX < mainDot.mPosX) angle = 90 + (rand() % 90); // 90 đến 180
+                else angle = rand() % 90; // 0 đến 90
+            }
+            else if (mainDot.mVelY > 0) { // Main dot đi xuống
+                if (mPosX < mainDot.mPosX) angle = 180 + (rand() % 90); // 180 đến 270
+                else angle = 270 + (rand() % 90); // 270 đến 360
+            }
+            else if (mainDot.mVelX > 0) { // Main dot đi sang phải
+                if (mPosY > mainDot.mPosY) angle = 270 + (rand() % 90); // 270 đến 360
+                else angle = rand() % 90; // 0 đến 90
+            }
+            else if (mainDot.mVelX < 0) { // Main dot đi sang trái
+                if (mPosY > mainDot.mPosY) angle = 180 + (rand() % 90); // 180 đến 270
+                else angle = 90 + (rand() % 90); // 90 đến 180
+            }
+
+            // Chuyển đổi góc từ độ sang radian
+            double radian = angle * (M_PI / 180.0);
+
+            // Cập nhật vị trí của dot phụ
+            mPosX += speed * cos(radian);
+            mPosY += speed * sin(radian);
+        }
+
+        // *** Xử lý va chạm giữa các dot phụ ***
+        for (Dot &other : dots) {
+            if (&other == this || other.mIsMainDot) continue; // Không xét chính nó hoặc main dot
+
+            double d = getDistance(mPosX, mPosY, other.mPosX, other.mPosY);
+
+            if (d < RANGE) { // Nếu hai dot phụ chạm nhau
+                double overlap = RANGE - d;
+				if (d > 1e-5) {
+					double dx = (mPosX - other.mPosX) / d;
+					double dy = (mPosY - other.mPosY) / d;
+
+					// Đẩy hai dot phụ ra xa để tránh chồng lên nhau
+					mPosX += dx * overlap * 0.02;
+					mPosY += dy * overlap * 0.02;
+					other.mPosX -= dx * overlap * 0.02;
+					other.mPosY -= dy * overlap * 0.02;
+				}
+            }
+        }
+
+		if (mPosX < 0) mPosX = 0;
+		if (mPosX + DOT_WIDTH > SCREEN_WIDTH) mPosX = SCREEN_WIDTH - DOT_WIDTH;
+		if (mPosY < 0) mPosY = 0;
+		if (mPosY + DOT_HEIGHT > SCREEN_HEIGHT) mPosY = SCREEN_HEIGHT - DOT_HEIGHT;
+
     }
 }
 
@@ -463,7 +577,12 @@ int main( int argc, char* args[] )
 			SDL_Event e;
 
 			//The dot that will be moving around on the screen
-			Dot dot;
+			Dot mainDot(true);  // Dot chính do người chơi điều khiển
+    		std::vector<Dot> dots; // Danh sách dot phụ
+			for (int i = 0; i < 5; i++)
+			{
+				dots.push_back(Dot(false));
+			}
 
 			//While application is running
 			while( !quit )
@@ -478,18 +597,26 @@ int main( int argc, char* args[] )
 					}
 
 					//Handle input for the dot
-					dot.handleEvent( e );
+					mainDot.handleEvent( e );
 				}
 
 				//Move the dot
-				dot.move();
+				mainDot.move(mainDot, dots);
+				for (auto &dot : dots)
+				{
+					dot.move(mainDot, dots);
+				}
 
 				//Clear screen
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
 				//Render objects
-				dot.render();
+				mainDot.render();
+				for (auto &dot : dots)
+				{
+					dot.render();
+				}
 
 				//Update screen
 				SDL_RenderPresent( gRenderer );
